820837bf2c16d921e126e084614f0f8c01f7559b
Mohcka <mikeduels@yahoo.com>
2020-06-12 16:40:08 -0700

    Updates tests 🧪

--- angular/src/app/modules/booking/booking/booking.component.spec.ts
+++ angular/src/app/modules/booking/booking/booking.component.spec.ts
@@ -1,17 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookingComponent } from './booking.component';
-import { FormBuilder } from '@angular/forms';
-import { HttpClientTestingModule} from '@angular/common/http/testing'
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BookingModalComponent } from '../booking-modal/booking-modal.component';
 
-describe('BookingComponent', () => {
+fdescribe('BookingComponent', () => {
   let component: BookingComponent;
   let fixture: ComponentFixture<BookingComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BookingComponent],
-      providers: [FormBuilder,],
-      imports: [HttpClientTestingModule,]
+      declarations: [BookingComponent, BookingModalComponent],
+      providers: [FormBuilder],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
     }).compileComponents();
   }));
 
@@ -24,4 +25,12 @@ describe('BookingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain "Guests"', () => {
+    const bookingEl: HTMLElement = fixture.nativeElement;
+    
+    expect(bookingEl.textContent).toContain('Guest');
+  });
+
+  it('should ')
 });
