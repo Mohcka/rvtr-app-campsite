e8e9789b167ea7c453ae0022b6aad2d121fe40fc
Mohcka <mikeduels@yahoo.com>
2020-06-13 16:42:40 -0700

    Adds test 🧪

--- angular/src/app/modules/booking/booking-modal/booking-modal.component.spec.ts
+++ angular/src/app/modules/booking/booking-modal/booking-modal.component.spec.ts
@@ -20,8 +20,6 @@ describe('BookingModalComponent', () => {
   let expectedLodging: Lodging;
   let expectedBookingSearchData: BookingSearchData;
 
-
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [BookingModalComponent],
@@ -61,5 +59,49 @@ describe('BookingModalComponent', () => {
   it('createGuestItem should return FromGroup', () => {
     const guestItem: FormGroup | FormGroup[] = component.createGuestItem();
     expect(guestItem).toBeInstanceOf(FormGroup);
+  });
+
+  it('should add a guest item', () => {
+    // * Arrange
+    // Guest form groups should equal to the number of
+    // guests chosen from BookingSearchData Input
+    const initialNumGuests = expectedBookingSearchData.guests.value;
+    const numToAdd = 3;
+
+    // * Act
+    // Add new guest form groups
+
+    // Looping method taken from:
+    // https://stackoverflow.com/a/39232049
+    Array.from(Array(numToAdd)).forEach(() => {
+      component.addNextGuestItem();
+    });
+
+    // * Assert
+    // Number of guest form groups should be incremented
+    const resultNumGuestFormGroups = (component.bookingForm.controls['guests'] as FormArray).length;
+    expect(resultNumGuestFormGroups).toEqual(initialNumGuests + numToAdd);
+  });
+
+  it('should remove guest form group', () => {
+    // * Arrange
+    // Guest form groups should equal to the number of
+    // guests chosen from BookingSearchData Input
+    const initialNumGuests = expectedBookingSearchData.guests.value;
+    const numToAdd = initialNumGuests + 3;
+    // * Act
+    // First add a few form groups
+
+    // Looping method taken from:
+    // https://stackoverflow.com/a/39232049
+    Array.from(Array(numToAdd)).forEach(() => {
+      component.addNextGuestItem();
+    });
+
+    // Remove one
+    component.removeGuestItem(0);
+    // * Assert
+    const resultNumGuestFormGroups = (component.bookingForm.controls['guests'] as FormArray).length;
+    expect(resultNumGuestFormGroups).toBe(numToAdd - 1);
   });
 });
