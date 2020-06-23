aba10f72b9c80d0a1e9a5d1648c753d7d03ea932
kenendo12 <kenendo12@gmail.com>
2020-06-22 22:48:59 -0500

    adds account changes

--- angular/src/app/data/accountBooking.ts
+++ angular/src/app/data/accountBooking.ts
@@ -3,5 +3,5 @@ import { Booking } from './booking.model';
 import { Lodging } from './lodging.model';
 
 export interface AccountBooking extends Booking{
-    lodging: Lodging;
+    lodging?: Lodging;
 }
--- angular/src/app/modules/account/display-bookings/display-bookings.component.ts
+++ angular/src/app/modules/account/display-bookings/display-bookings.component.ts
@@ -22,15 +22,15 @@ export class DisplayBookingsComponent implements OnInit {
   // The 2 listings serve as a quick snapshot accessible from the account dashboard.
   // Bookings will be sorted on the API end. using account id.
   getBookings() {
-//    this.accountService.getBookings(this.accountService.getUserId(), new HttpParams().set('sort', 'id desc'))
-//      .pipe(map(bookings => bookings.slice(0, 2)))
-//      .subscribe(resultBookings => {
-//        this.bookings = resultBookings; console.log(this.bookings);
-//        this.bookings.forEach(booking =>
-//          this.lodgingService.get(booking.lodgingId.toString())
-//                             .subscribe(lodging =>
-//                                booking.lodging = lodging[0]));
-//      });
+   this.bookingService.get(undefined, new HttpParams().set('sort', 'id desc')
+   .set('accountId', this.accountService.getUserId()).set('limit', '2'))
+     .subscribe(resultBookings => {
+       this.bookings = resultBookings;
+       this.bookings.forEach(booking =>
+         this.lodgingService.get(booking.lodgingId.toString())
+                            .subscribe((lodging: any) =>
+                               booking.lodging = lodging));
+     });
   }
 
   constructor(
